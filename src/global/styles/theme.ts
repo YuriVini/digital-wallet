5cd7648245353066ecaa587ae6b3935894b6d77b
Yuri Vinicius Ferreira Oliveira <yuri.ybgt.yfo@gmail.com>
2022-01-30 12:44:59 -0300

    Global Colors

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,13 @@
+export default {
+  colors: {
+    white: "#FFFFFF",
+    grayLight: "#D2D2D2",
+    black: "#000000",
+
+    primary: "#20CAA8",
+
+    textLight: "#989898",
+
+    danger: "#EB0E0F",
+  },
+};
