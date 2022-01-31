cf9a5be778fcae80cd585f3b8098cddf9f8caea0
Yuri Vinicius Ferreira Oliveira <yuri.ybgt.yfo@gmail.com>
2022-01-30 21:30:32 -0300

    Services API

---
+++ src/services/DigitalWalletAPI.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { DIGITAL_WALLET_API } from "./consts";
+
+export type GetMyBalance = {
+  saldo: number;
+};
+
+export type GetMyTransactions = {
+  id: number;
+  descricao: string;
+  dataTransacao: string;
+  tipoTransacao: string;
+  valorTransacao: number;
+  tipoLancamento: string;
+  cartaoMascarado: string;
+  finalCartao: string;
+  nomePortador: string;
+};
+
+const digitalWalletAPI = axios.create({
+  baseURL: DIGITAL_WALLET_API,
+  timeout: 10000,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const getMyBalance = () =>
+  digitalWalletAPI.get<GetMyBalance>("/balance");
+
+export const getMyTransactions = () =>
+  digitalWalletAPI.get<GetMyTransactions[]>("/transactions");
---
+++ src/services/consts.ts
@@ -0,0 +1,2 @@
+export const DIGITAL_WALLET_API =
+  "https://5efb30ac80d8170016f7613d.mockapi.io/api/mock";
