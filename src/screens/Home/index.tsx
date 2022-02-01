04e4cf810a19f74e1e9dde7ce8f588c23cfac19a
Yuri Vinicius Ferreira Oliveira <yuri.ybgt.yfo@gmail.com>
2022-02-01 10:22:37 -0300

    Remove some code

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { View } from "react-native";
 import { Fontisto, SimpleLineIcons } from "@expo/vector-icons";
 import Wrapper from "../../components/Wrapper";
 
